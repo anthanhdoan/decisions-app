5cc704dedb702611a7c22feac10817f2794cec0d
Ân Doan <82601667+anthanhdoan@users.noreply.github.com>
2025-07-05 13:55:03 +0200

    The first few steps - removed some boiler plate and added a html/css navbar

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <nav className="nav-bar">
+        <div className="nav-logo">DecisionsApp</div>
+        <ul>
+          <li className="nav-item">Link1</li>
+          <li className="nav-item">Link2</li>
+          <li className="nav-item">Link3</li>
+        </ul>
+      </nav>
+
+      <div>
+        <h1>D3 app</h1>
+        <div className="card">
+          <h1 className="card-title">Card Title</h1>
+          <p className="card-body">
+            Card body Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Sit alias architecto quam, modi voluptate optio fuga aspernatur
+            earum repellat est, nemo sunt itaque. Iste sint est perferendis ut,
+            exercitationem natus?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
