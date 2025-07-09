ea1c150cdbfcc12a728d8b170446d6d84a2de0e2
Ân Doan <82601667+anthanhdoan@users.noreply.github.com>
2025-07-09 15:00:17 +0200

    Made NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 // import Card from "./components/Card";
 import OptionsList from "./components/OptionsList";
 
 function App() {
   return (
     <div className="container">
-      <nav className="nav-bar">
-        <div className="nav-logo">DecisionsApp</div>
-        <ul>
-          <li className="nav-item">Link1</li>
-          <li className="nav-item">Link2</li>
-          <li className="nav-item">Link3</li>
-        </ul>
-      </nav>
-
+      <NavBar></NavBar>
       <h1>D3 app</h1>
       <OptionsList></OptionsList>
       {/* <Card></Card> */}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <div className="nav-logo">DecisionsApp</div>
+      <ul>
+        <li className="nav-item">Link1</li>
+        <li className="nav-item">Link2</li>
+        <li className="nav-item">Link3</li>
+      </ul>
+    </nav>
+  );
+}
